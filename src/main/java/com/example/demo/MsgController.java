be658d2dd8d7ab872300f203e93d54a3419d42fe
abhishek8123 <abhi19patil91@gmail.com>
2023-07-31 21:31:02 +0530

    first commit

---
+++ src/main/java/com/example/demo/MsgController.java
@@ -0,0 +1,14 @@
+package com.example.demo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MsgController {
+	
+	@GetMapping("/hai")
+	public String msg() {
+		return "hai abhishek";
+	}
+
+}
